32af76b3ece76d4428ebf69359dafc1c4f179f65
Andrea Guffi <andreaguffi92@gmail.com>
2021-03-20 08:44:41 +0100

    save photo to phone memory

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -11,12 +13,30 @@ class _ImageInputState extends State<ImageInput> {
   File _storedImage; //img is saved on the mobile device memory
 
   Future<void> _takePicture() async {
-    //final imageFile = await  ImagePicker.pickImage();
-    final picker = ImagePicker();
-    final imageFile = await picker.getImage(
+    // old
+    final imageFile = await ImagePicker.pickImage(
       source: ImageSource.camera,
       maxWidth: 600,
     );
+
+    //new
+    //final picker = ImagePicker();
+    //final imageFile = await picker.getImage(
+    //  source: ImageSource.camera,
+    //  maxWidth: 600,
+    //);
+    setState(() {
+      // old
+      _storedImage = imageFile;
+
+      // new
+      //_storedImage:
+      //File(imageFile.path);
+    });
+    // save the file into phone location
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/$filename');
   }
 
   @override
